16b7100af7305ea7d7f27a93923dee8dd0395957
YassGass <Yhassan.J@protonmail.com>
2021-10-06 01:48:09 +0200

    second commit

---
+++ syntaxeJavascript/assets/js/script.js
@@ -0,0 +1,14 @@
+//l'instruction qui affiche test sur la console
+console.log("test");
+
+//console.log("test"); ligne de code commenter
+
+/*
+
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+*/
---
+++ variable/assets/js/script.js
@@ -0,0 +1,10 @@
+//déclaration d'une variable
+var age;
+
+age= 23; // sauvegarder les numbers
+
+var papa;
+
+papa = "joyboy"; // sauvegarder les strings
+
+var float = 1.2;  // sauvegarder les floats
